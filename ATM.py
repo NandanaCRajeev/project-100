72b738e2847b4d413ee54782eb78027f1e8a121d
NandanaCRajeev <83021680+NandanaCRajeev@users.noreply.github.com>
2021-11-21 17:59:31 +0530

    Add files via upload

---
+++ ATM.py
@@ -0,0 +1,17 @@
+class ATM(object):
+    def __init__(self,cardnumber,pinnumber,):
+        self.cardnumber = cardnumber
+        self.pinnumber = pinnumber
+        
+
+    def CashWithDrawl(self):
+        print("CashWithDrawl")
+    def BalanceEnquiry(self):
+        print("BalanceEnquiry")
+
+    if(activity == 1):
+        new_user.check_balance()
+        elif (activity == 2): amount = int(input("enter the amount:- "))
+        new_user.withdrawl(amount) else: print("enter a valid number")
+   
+        
